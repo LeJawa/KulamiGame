a90061fc37c1b2543abaee771f0a9f9f468588e1
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-06-21 17:55:22 +0200

    Camera centers on board

--- Assets/Scripts/GameDrawer.cs
+++ Assets/Scripts/GameDrawer.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,6 +24,9 @@ public class GameDrawer : MonoBehaviour
 
     private GameObject _playerOneBall;
     private GameObject _playerTwoBall;
+
+    [SerializeField]
+    private Camera _camera;
 
     public void Awake()
     {
@@ -50,12 +54,33 @@ public class GameDrawer : MonoBehaviour
     {
         var result = new GameObject[tiles.Length];
 
+        var minX = int.MaxValue;
+        var maxX = int.MinValue;
+        var minY = int.MaxValue;
+        var maxY = int.MinValue;
+
         foreach (UnitTile tile in tiles)
         {
             // Instantiate and Initialize in one line
             // Smells dirty but will reformat later
             Instantiate(_unitTilePrefab, tile.Position.ToVector3(), Quaternion.identity).GetComponent<UnitTileGO>().Initialize(tile);
+
+            if (minX > tile.Position.x) minX = tile.Position.x;
+            if (maxX < tile.Position.x) maxX = tile.Position.x;
+            if (minY > tile.Position.y) minY = tile.Position.y;
+            if (maxY < tile.Position.y) maxY = tile.Position.y;
         }
+
+        // We add maxX and minX together because minX is always negative
+        float meanX = (maxX + 1 + minX) / 2f;
+        float meanY = (maxY + 1 + minY) / 2f;
+
+        SetCamera(meanX, meanY);
+    }
+
+    private void SetCamera(float centerX, float centerY)
+    {
+        _camera.transform.position = new Vector3(centerX, centerY, _camera.transform.position.z);
     }
 
     public void DrawGameTile(Tile tile)
